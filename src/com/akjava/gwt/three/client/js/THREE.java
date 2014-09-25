f755364c0c3c732462b452dec428fe6791358050
sebasbaumh <sebastian.baumhekel@gmail.com>
2014-09-25 09:08:05 +0200

    integrate TrackballControls.

--- src/com/akjava/gwt/three/client/js/THREE.java
+++ src/com/akjava/gwt/three/client/js/THREE.java
@@ -66,6 +66,7 @@ import com.akjava.gwt.three.client.js.extras.animation.AnimationMorphTarget;
 import com.akjava.gwt.three.client.js.extras.animation.KeyFrameAnimation;
 import com.akjava.gwt.three.client.js.extras.cameras.CombinedCamera;
 import com.akjava.gwt.three.client.js.extras.cameras.CubeCamera;
+import com.akjava.gwt.three.client.js.extras.controls.TrackballControls;
 import com.akjava.gwt.three.client.js.extras.core.Gyroscope;
 import com.akjava.gwt.three.client.js.extras.core.Path;
 import com.akjava.gwt.three.client.js.extras.core.Shape;
@@ -889,6 +890,25 @@ return new $wnd.THREE.ArrowHelper(size);
 	return new $wnd.THREE.Projector();
 	}-*/;
 	
+	/**
+	 * Constructs a new {@link TrackballControls} instance.
+	 * @param object object to move/rotate
+	 * @param domElement element (can be null)
+	 * @return {@link TrackballControls} instance
+	 */
+	public static native final TrackballControls TrackballControls(Object3D object,
+			com.google.gwt.dom.client.Element domElement)/*-{
+		return new $wnd.THREE.TrackballControls(object, domElement);
+	}-*/;
+
+	/**
+	 * Constructs a new {@link TrackballControls} instance.
+	 * @param object object to move/rotate
+	 * @return {@link TrackballControls} instance
+	 */
+	public static native final TrackballControls TrackballControls(Object3D object)/*-{
+		return new $wnd.THREE.TrackballControls(object);
+	}-*/;
 	
 	/*
 	public static final int FrontSide = 0;
---
+++ src/com/akjava/gwt/three/client/js/extras/controls/TrackballControls.java
@@ -0,0 +1,140 @@
+package com.akjava.gwt.three.client.js.extras.controls;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.ui.HasEnabled;
+
+/**
+ * Controls for changing the position and angle of an object, e.g. a camera.
+ * @author sbaumhekel
+ */
+public class TrackballControls extends JavaScriptObject implements HasEnabled
+{
+
+	/**
+	 * Do not use.
+	 */
+	@Deprecated
+	protected TrackballControls()
+	{
+	}
+
+	/**
+	 * Gets whether panning is allowed.
+	 * @return true on success, else false
+	 */
+	public final native double getAllowPan()/*-{
+		return !this.noPan;
+	}-*/;
+
+	/**
+	 * Gets whether rotation is allowed.
+	 * @return true on success, else false
+	 */
+	public final native double getAllowRotate()/*-{
+		return !this.noRotate;
+	}-*/;
+
+	/**
+	 * Gets whether zoom is allowed.
+	 * @return true on success, else false
+	 */
+	public final native double getAllowZoom()/*-{
+		return !this.noZoom;
+	}-*/;
+
+	/**
+	 * Gets the maximum distance of objects to be shown.
+	 * @return maximum distance (default: {@link Double#POSITIVE_INFINITY} )
+	 */
+	public final native double getMaxDistance()/*-{
+		return this.maxDistance;
+	}-*/;
+
+	/**
+	 * Gets the minimum distance of objects to be shown.
+	 * @return minimum distance (default: 0)
+	 */
+	public final native double getMinDistance()/*-{
+		return this.minDistance;
+	}-*/;
+
+	/**
+	 * Handles resizes of the parent element.
+	 */
+	public final native void handleResize()/*-{
+		this.handleResize();
+	}-*/;
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.google.gwt.user.client.ui.HasEnabled#isEnabled()
+	 */
+	@Override
+	public final native boolean isEnabled()/*-{
+		return this.enabled;
+	}-*/;
+
+	/**
+	 * Resets the current control state.
+	 */
+	public final native void reset()/*-{
+		this.reset();
+	}-*/;
+
+	/**
+	 * Sets whether panning is allowed.
+	 * @param allow allow panning
+	 */
+	public final native void setAllowPan(boolean allow)/*-{
+		this.noPan = !allow;
+	}-*/;
+
+	/**
+	 * Sets whether rotation is allowed.
+	 * @param allow allow rotation
+	 */
+	public final native void setAllowRotate(boolean allow)/*-{
+		this.noRotate = !allow;
+	}-*/;
+
+	/**
+	 * Sets whether zoom is allowed.
+	 * @param allow allow zoom
+	 */
+	public final native void setAllowZoom(boolean allow)/*-{
+		this.noZoom = !allow;
+	}-*/;
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.google.gwt.user.client.ui.HasEnabled#setEnabled(boolean)
+	 */
+	@Override
+	public final native void setEnabled(boolean enabled)/*-{
+		this.enabled = enabled;
+	}-*/;
+
+	/**
+	 * Sets the maximum distance of objects to be shown.
+	 * @param maxDistance maximum distance {@link Double#POSITIVE_INFINITY}
+	 */
+	public final native void setMaxDistance(double maxDistance)/*-{
+		this.maxDistance = maxDistance;
+	}-*/;
+
+	/**
+	 * Sets the minimum distance of objects to be shown.
+	 * @param minDistance minimum distance (default: 0)
+	 */
+	public final native void setMinDistance(double minDistance)/*-{
+		this.minDistance = minDistance;
+	}-*/;
+
+	/**
+	 * Updates the current control state.
+	 */
+	public final native void update()/*-{
+		this.update();
+	}-*/;
+
+}
